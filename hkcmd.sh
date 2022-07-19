a74e263c72df94b1e1c6e98fab4f647e1b810b0e
falcon <falcon_chen@qq.com>
2022-07-19 19:45:57 +0800

    init

---
+++ hkcmd.sh
@@ -0,0 +1,10 @@
+hkcmd(){ssh root@txhk "$@"}
+ytcy(){hkcmd yt-cy "$@"}
+yt-all(){hkcmd yt-all "$@"}
+yt-ali(){hkcmd yt-ali "$@"}
+yt-cy(){hkcmd yt-cy "$@"}
+nyt-cy(){hkcmd nyt-cy "$@"}
+nytcy(){hkcmd nyt-cy "$@"}
+nyta-cy(){hkcmd nyta-cy "$@"}
+nytacy(){hkcmd nyta-cy "$@"}
+
---
+++ n1cmd.sh
@@ -0,0 +1,12 @@
+n1cmd(){ssh root@n1 "$@"}
+yt(){n1cmd yt "$@"}
+yt-mp4(){n1cmd yt-mp4 "$@"}
+yta(){n1cmd yta "$@"}
+ytc(){n1cmd ytc "$@"}
+ytcc(){n1cmd ytcc "$@"}
+ytai(){n1cmd ytai "$@"}
+yta-mp3(){n1cmd yta-mp3 "$@"}
+n1yt-ali(){n1cmd n1yt-ali "$@"}
+n1yt(){n1cmd n1yt "$@"}
+
+
