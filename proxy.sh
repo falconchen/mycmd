4d3a0fb7e260cff795020d0aaba09aac7a16e3aa
falcon <falcon_chen@qq.com>
2022-08-10 15:29:03 +0800

    update proxy setting

--- proxy.sh
+++ proxy.sh
@@ -1,6 +1,6 @@
 #export http_proxy="socks5://127.0.0.1:1085"
 #export ALL_PROXY=socks5://127.0.0.1:1080
 
-alias setproxy="export ALL_PROXY=socks5://127.0.0.1:1085"
-alias unsetproxy="unset ALL_PROXY"
+alias proxy="export ALL_PROXY=socks5://127.0.0.1:1085"
+alias unproxy="unset ALL_PROXY"
 
